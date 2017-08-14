c56fbc546904bf851a08a6d6d4271db90db0c2d1
Ray Koren <raykoren@gmail.com>
2017-08-14 15:22:26 -0600

    Added content

--- app.js
+++ app.js
@@ -14,15 +14,55 @@ angular.module('psychFest', [
                 controller: 'homeController'
             })
             .when('/events', {
-                templateUrl : 'templates/events.html',
-                controller  : 'homeController'
+                templateUrl: 'templates/events.html',
+                controller: 'homeController'
             })
-
-            // route for the contact page
             .when('/contact', {
-                templateUrl : 'templates/contact.html',
-                controller  : 'homeController'
+                templateUrl: 'templates/contact.html',
+                controller: 'homeController'
+            })
+            .when('/tickets', {
+                templateUrl: 'templates/tickets.html',
+                controller: 'homeController'
+            })
+            .when('/audio', {
+                templateUrl: 'templates/audio.html',
+                controller: 'homeController'
+            })
+            .when('/video', {
+                templateUrl: 'templates/video.html',
+                controller: 'homeController'
+            })
+            .when('/privacy', {
+                templateUrl: 'templates/privacy.html',
+                controller: 'homeController'
+            })
+            .when('/press', {
+                templateUrl: 'templates/press.html',
+                controller: 'homeController'
+            })
+            .when('/2013', {
+                templateUrl: 'templates/2013.html',
+                controller: 'homeController'
+            })
+            .when('/2014', {
+                templateUrl: 'templates/2014.html',
+                controller: 'homeController'
+            })
+            .when('/2015', {
+                templateUrl: 'templates/2015.html',
+                controller: 'homeController'
+            })
+            .when('/2017', {
+                templateUrl: 'templates/2017.html',
+                controller: 'homeController'
+            })
+            .when('/terms', {
+                templateUrl: 'templates/terms.html',
+                controller: 'homeController'
+            })
+            .when('/images', {
+                templateUrl: 'templates/images.html',
+                controller: 'homeController'
             });
-   
     });
-
