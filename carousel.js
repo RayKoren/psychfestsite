eed30b037d6fa9605f65559cbfa0fa2daa8a217c
Ray Koren <raykoren@gmail.com>
2017-08-15 16:37:14 -0600

    Added home Carousel fixed styles

--- carousel.js
+++ carousel.js
@@ -1,26 +1,26 @@
-$(document).ready(function(){
-    // Activate Carousel
-    $("#carouselHeader").carousel();
-
-    // Enable Carousel Indicators
-    $(".item1").click(function(){
-        $("#carouselHeader").carousel(0);
-    });
-    $(".item2").click(function(){
-        $("#carouselHeader").carousel(1);
-    });
-    $(".item3").click(function(){
-        $("#carouselHeader").carousel(2);
-    });
-    $(".item4").click(function(){
-        $("#carouselHeader").carousel(3);
-    });
-
-    // Enable Carousel Controls
-    $(".left").click(function(){
-        $("#carouselHeader").carousel("prev");
-    });
-    $(".right").click(function(){
-        $("#carouselHeader").carousel("next");
-    });
-});
+//$(document).ready(function(){
+//    // Activate Carousel
+//    $("#carouselHeader").carousel();
+//
+//    // Enable Carousel Indicators
+//    $(".item1").click(function(){
+//        $("#carouselHeader").carousel(0);
+//    });
+//    $(".item2").click(function(){
+//        $("#carouselHeader").carousel(1);
+//    });
+//    $(".item3").click(function(){
+//        $("#carouselHeader").carousel(2);
+//    });
+//    $(".item4").click(function(){
+//        $("#carouselHeader").carousel(3);
+//    });
+//
+//    // Enable Carousel Controls
+//    $(".left").click(function(){
+//        $("#carouselHeader").carousel("prev");
+//    });
+//    $(".right").click(function(){
+//        $("#carouselHeader").carousel("next");
+//    });
+//});
