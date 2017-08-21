5b2f4e5e0501953b8ec43e56c71f7af49cabc32f
Ray Koren <raykoren@gmail.com>
2017-08-21 16:53:54 -0600

    Updated slider function and fixed event page styles

--- carousel.js
+++ carousel.js
@@ -1,38 +1,16 @@
-$(document).ready(function(){
-carouselNormalization();
-function carouselNormalization() {
-  console.log('hi');
-var items = $('#carouselHeader .item'), //grab all slides
-    heights = [], //create empty array to store height values
-    tallest; //create variable to make note of the tallest slide
-    function normalizeHeights() {
-        items.each(function() { //add heights to array
-            heights.push($(this).height());
-        });
-        tallest = Math.max.apply(null, heights); //cache largest value
-        items.each(function() {
-            $(this).css('min-height',tallest + 'px');
-        });
-    }
-if (items.length) {
-
-    normalizeHeights();
-
-    $(window).on('resize orientationchange', function () {
-        tallest = 0, heights.length = 0; //reset vars
-        items.each(function() {
-            $(this).css('min-height','0'); //reset min-height
-        });
-        normalizeHeights(); //run it again
-    });
-}
-}
-
-   // Enable Carousel Controls
-   $(".left").click(function(){
-       $("#carouselHeader").carousel("prev");
-   });
-   $(".right").click(function(){
-       $("#carouselHeader").carousel("next");
-   });
+$(document).ready(function() {
+  // var items = $('#carouselHeader .carousel-item'); //grab all slides
+  // function carouselNormalization() {
+  //   items.each(function() {
+  //     $(this).css('min-height', 500 + 'px');
+  //   });
+  // }
+  // carouselNormalization();
+  // Enable Carousel Controls
+  $(".left").click(function() {
+    $("#carouselHeader").carousel("prev");
+  });
+  $(".right").click(function() {
+    $("#carouselHeader").carousel("next");
+  });
 });
